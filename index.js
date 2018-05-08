71997134c3e7e8cb81179d1d841678fe0954de14
pegasuspect <osmansekerlentravel@yandex.com>
2018-05-07 22:29:17 -0400

    Started using sockets!

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket) {
+  console.log("some dushbag just used our socket.");
+  socket.on("disconnect", function() {
+    console.log("He fucking left!");
+  })
+})
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
