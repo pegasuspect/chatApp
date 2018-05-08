cf8b82791392f426db7c02aa4ba1f43369949e09
pegasuspect <1279406+pegasuspect@users.noreply.github.com>
2018-05-08 14:13:22 -0400

    Broadcast a message to connected users when someone connects or disconnects

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket) {
-  console.log("some dushbag just used our socket.");
+  io.emit('notification', 'A dushbag connected!');
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   })
   socket.on("disconnect", function() {
-    console.log("He fucking left!");
+    io.emit('notification', 'A dushbag left.');
   })
 })
 
