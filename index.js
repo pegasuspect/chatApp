9ecab9d1a6ad224662adf14e84c2c4926239259c
pegasuspect <osmansekerlentravel@yandex.com>
2018-05-08 18:18:08 -0400

    Add support for nicknames. Session support. Credit: https://www.npmjs.com/package/express-socket.io-session

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var session = require("express-session")({
+    secret: "my-secret",
+    resave: true,
+    saveUninitialized: true
+  });
+var sharedsession = require("express-socket.io-session");
+
+// Attach session
+app.use(session);
+
+// Share session with io sockets
+io.use(sharedsession(session));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-  io.emit('notification', 'A dushbag connected!');
+  // Accept a login event with user's data
+  socket.on("login", function(userdata) {
+    socket.handshake.session.userdata = userdata;
+    var nick = userdata.nickname;
+    io.emit('notification', nick + " connected!");
+    socket.handshake.session.save();
+    console.log(socket.handshake);
+  });
+  // if logout is emitted use:
+  // socket.on("logout", function(userdata) {});
+
+  // io.on('connection', function(socket){
+  // });
+
+  socket.on('notification', function(msg) {
+    io.emit('notification', msg);
+  });
+
   socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
+    var userdata = socket.handshake.session.userdata;
+    io.emit('chat message', {userdata, msg});
   })
-  socket.on("disconnect", function() {
-    io.emit('notification', 'A dushbag left.');
-  })
+  socket.on('disconnect', function(){
+    var nick = socket.handshake.session.userdata.nickname;
+    io.emit('notification', nick + " left!");
+    if (socket.handshake.session.userdata) {
+      delete socket.handshake.session.userdata;
+      socket.handshake.session.save();
+    }
+  });
 })
 
-http.listen(3000, function(){
+server.listen(3000, function(){
   console.log('listening on *:3000');
 });
